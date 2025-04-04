7089f74bf1bc627bbdf513758186039d60d056ea
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-04 23:21:31 +0530

    Updated features and fixed bugs

--- src/main/java/com/ServiceBookingSystem/SERVICES/Company/CompanyService.java
+++ src/main/java/com/ServiceBookingSystem/SERVICES/Company/CompanyService.java
@@ -7,9 +7,13 @@ import java.io.IOException;
 import java.util.List;
 
 public interface CompanyService {
+
     boolean postAd(Long userId, AdDTO adDTO) throws IOException;
+
     List<AdDTO> getAllAds(Long userId);
+
     AdDTO getAdById(Long adId);
+
     boolean updateAd(Long adId,AdDTO adDTO) throws IOException;
     boolean deleteAd(Long adId);
     List<ReservationDTO> getAllAdBookings(Long companyId);
