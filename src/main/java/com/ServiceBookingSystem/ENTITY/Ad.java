7b27f1fc4e5b188b83227ee20f373992df2c7fda
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-04 23:01:57 +0530

    Updated features and fixed bugs

--- src/main/java/com/ServiceBookingSystem/ENTITY/Ad.java
+++ src/main/java/com/ServiceBookingSystem/ENTITY/Ad.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+
 @Entity
 @Table(name = "ads")
 @Data
@@ -12,19 +13,13 @@ public class Ad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String serviceName;
-
     private String description;
-
     private Double price;
-
 
     @Lob
     @Column(columnDefinition = "longblob")
     private byte[] img;
-
-
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id")
@@ -33,12 +28,15 @@ public class Ad {
 
     public AdDTO getAdDto() {
         AdDTO adDTO = new AdDTO();
+
         adDTO.setId(id);
         adDTO.setServiceName(serviceName);
         adDTO.setDescription(description);
         adDTO.setPrice(price);
         adDTO.setCompanyName(user.getName());
         adDTO.setReturnedImg(img);
+
         return adDTO;
     }
+
 }
