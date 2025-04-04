ffca90001621cc4805930b42b951051dd950f7de
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-04 23:10:49 +0530

    Updated features and fixed bugs

--- src/main/java/com/ServiceBookingSystem/REPOSITORY/ReservationRepository.java
+++ src/main/java/com/ServiceBookingSystem/REPOSITORY/ReservationRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
     List<Reservation> findAllByCompanyId(Long CompanyId);
+
     List<Reservation> findAllByUserId(Long userId);
+
 }
