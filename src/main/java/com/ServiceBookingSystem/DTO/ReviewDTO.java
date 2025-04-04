aa48ebfb1405075ff1d7513193772fe0f6df9070
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-04 23:07:01 +0530

    Updated features and fixed bugs

--- src/main/java/com/ServiceBookingSystem/DTO/ReviewDTO.java
+++ src/main/java/com/ServiceBookingSystem/DTO/ReviewDTO.java
@@ -6,13 +6,23 @@ import lombok.Data;
 import java.util.Date;
 @Data
 public class ReviewDTO {
+
     private Long id;
+
     private String review;
+
     private Date reviewDate;
+
     private Long rating;
+
     private Long userId;
+
     private Long adId;
+
     private String clientName;
+
     private String serviceName;
+
     private Long bookId;  // Make sure this is properly included
+
 }
