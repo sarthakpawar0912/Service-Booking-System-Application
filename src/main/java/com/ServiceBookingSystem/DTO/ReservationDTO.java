fbdf5a8b096f160679fa5adaf109b20940406507
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-04 23:06:46 +0530

    Updated features and fixed bugs

--- src/main/java/com/ServiceBookingSystem/DTO/ReservationDTO.java
+++ src/main/java/com/ServiceBookingSystem/DTO/ReservationDTO.java
@@ -9,13 +9,23 @@ import java.util.Date;
 
 @Data
 public class ReservationDTO {
+
     private Long id;
+
     private Date bookDate;
+
     private String serviceName;
+
     private ReservationStatus reservationStatus;
+
     private ReviewStatus reviewStatus;
+
     private Long userId;
+
     private String userName;
+
     private Long companyId;
+
     private Long adId;
+
 }
